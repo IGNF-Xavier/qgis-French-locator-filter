368177577c1e0e37cedd2b424da49b25f747757b
Julien M <julien.moura@oslandia.com>
2021-06-01 14:21:43 +0200

    Separate plugin from locator logic

---
+++ french_locator_filter/core/locator_filter.py
@@ -0,0 +1,138 @@
+#! python3  # noqa: E265
+
+"""
+    Main plugin module.
+"""
+
+# standard library
+import json
+
+# PyQGIS
+from qgis.core import (
+    Qgis,
+    QgsCoordinateReferenceSystem,
+    QgsCoordinateTransform,
+    QgsLocatorFilter,
+    QgsLocatorResult,
+    QgsMessageLog,
+    QgsPointXY,
+    QgsProject,
+)
+from qgis.PyQt.QtCore import pyqtSignal
+
+# project
+from french_locator_filter.toolbelt.networkaccessmanager import (
+    NetworkAccessManager,
+    RequestsException,
+)
+
+# ############################################################################
+# ########## Classes ###############
+# ##################################
+
+
+class FrenchBanGeocoderLocatorFilter(QgsLocatorFilter):
+
+    USER_AGENT = b"Mozilla/5.0 QGIS LocatorFilter"
+
+    SEARCH_URL = "https://api-adresse.data.gouv.fr/search/?limit=10&autocomplete=1&q="
+
+    resultProblem = pyqtSignal(str)
+
+    def __init__(self, iface):
+        self.iface = iface
+        super(QgsLocatorFilter, self).__init__()
+
+    def name(self):
+        return self.__class__.__name__
+
+    def clone(self):
+        return FrenchBanGeocoderLocatorFilter(self.iface)
+
+    def displayName(self):
+        return "Géocodeur API Adresse FR"
+
+    def prefix(self):
+        return "fra"
+
+    def fetchResults(self, search, context, feedback):
+
+        if len(search) < 2:
+            return
+
+        url = "{}{}".format(self.SEARCH_URL, search)
+        self.info("Search url {}".format(url))
+        nam = NetworkAccessManager()
+        try:
+
+            headers = {b"User-Agent": self.USER_AGENT}
+            # use BLOCKING request, as fetchResults already has it's own thread!
+            (response, content) = nam.request(url, headers=headers, blocking=True)
+
+            if (
+                response.status_code == 200
+            ):  # other codes are handled by NetworkAccessManager
+                content_string = content.decode("utf-8")
+                locations = json.loads(content_string)
+
+                # loop on features in json collection
+                for loc in locations["features"]:
+
+                    result = QgsLocatorResult()
+                    result.filter = self
+                    label = loc["properties"]["label"]
+                    if loc["properties"]["type"] == "municipality":
+                        # add city code to label
+                        label += " " + loc["properties"]["citycode"]
+                    result.displayString = label
+                    result.group = loc["properties"]["type"]
+                    # use the json full item as userData, so all info is in it:
+                    result.userData = loc
+                    self.resultFetched.emit(result)
+
+        except RequestsException as err:
+            # Handle exception..
+            self.info(err)
+            self.resultProblem.emit("{}".format(err))
+
+    def triggerResult(self, result):
+        self.info("UserClick: {}".format(result.displayString))
+        doc = result.userData
+        x = doc["geometry"]["coordinates"][0]
+        y = doc["geometry"]["coordinates"][1]
+
+        centerPoint = QgsPointXY(x, y)
+
+        dest_crs = QgsProject.instance().crs()
+        results_crs = QgsCoordinateReferenceSystem(
+            4326, QgsCoordinateReferenceSystem.PostgisCrsId
+        )
+        aTransform = QgsCoordinateTransform(
+            results_crs, dest_crs, QgsProject.instance()
+        )
+        centerPointProjected = aTransform.transform(centerPoint)
+        aTransform.transform(centerPoint)
+
+        # centers to adress coordinates
+        self.iface.mapCanvas().setCenter(centerPointProjected)
+
+        # zoom policy has we don't have extent in the results
+        scale = 25000
+
+        type_adress = doc["properties"]["type"]
+
+        if type_adress == "housenumber":
+            scale = 2000
+        elif type_adress == "street":
+            scale = 5000
+        elif type_adress == "locality":
+            scale = 5000
+
+        # finally zoom actually
+        self.iface.mapCanvas().zoomScale(scale)
+        self.iface.mapCanvas().refresh()
+
+    def info(self, msg=""):
+        QgsMessageLog.logMessage(
+            "{} {}".format(self.__class__.__name__, msg), "LocatorFilter", Qgis.Info
+        )
--- french_locator_filter/plugin_main.py
+++ french_locator_filter/plugin_main.py
@@ -1,144 +1,59 @@
-import json
+#! python3  # noqa: E265
 
-from qgis.core import (
-    Qgis,
-    QgsCoordinateReferenceSystem,
-    QgsCoordinateTransform,
-    QgsLocatorFilter,
-    QgsLocatorResult,
-    QgsMessageLog,
-    QgsPointXY,
-    QgsProject,
-)
-from qgis.PyQt.QtCore import pyqtSignal
+"""
+    Main plugin module.
+"""
 
-from .networkaccessmanager import NetworkAccessManager, RequestsException
+# PyQGIS
+from qgis.gui import QgisInterface
+
+# project
+from french_locator_filter.__about__ import __title__, __version__
+from french_locator_filter.core import FrenchBanGeocoderLocatorFilter
+from french_locator_filter.toolbelt import PlgLogger
+
+# ############################################################################
+# ########## Classes ###############
+# ##################################
 
 
 class LocatorFilterPlugin:
-    def __init__(self, iface):
+    def __init__(self, iface: QgisInterface):
+        """Constructor.
 
+        :param iface: An interface instance that will be passed to this class which \
+        provides the hook by which you can manipulate the QGIS application at run time.
+        :type iface: QgsInterface
+        """
         self.iface = iface
+        self.log = PlgLogger().log
 
-        self.filter = locatorFilter(self.iface)
-
+        # install locator filter
+        self.filter = FrenchBanGeocoderLocatorFilter(self.iface)
         self.filter.resultProblem.connect(self.show_problem)
         self.iface.registerLocatorFilter(self.filter)
 
-    def show_problem(self, err):
-        self.iface.messageBar().pushWarning(
-            "French Locator Filter Error", "{}".format(err)
-        )
+        if __debug__:
+            self.log(
+                message=(
+                    "DEBUG - French (BAN Geocoder) Locator Filter"
+                    f" ({__title__} {__version__}) installed."
+                ),
+                log_level=4,
+            )
 
     def initGui(self):
+        """Set up plugin UI elements."""
         pass
 
     def unload(self):
+        """Cleans up when plugin is disabled/uninstalled."""
         self.iface.deregisterLocatorFilter(self.filter)
 
+    def show_problem(self, err):
+        """Log errors.
 
-class locatorFilter(QgsLocatorFilter):
-
-    USER_AGENT = b"Mozilla/5.0 QGIS LocatorFilter"
-
-    SEARCH_URL = "https://api-adresse.data.gouv.fr/search/?limit=10&autocomplete=1&q="
-
-    resultProblem = pyqtSignal(str)
-
-    def __init__(self, iface):
-        self.iface = iface
-        super(QgsLocatorFilter, self).__init__()
-
-    def name(self):
-        return self.__class__.__name__
-
-    def clone(self):
-        return locatorFilter(self.iface)
-
-    def displayName(self):
-        return u"Géocodeur API Adresse FR"
-
-    def prefix(self):
-        return "fra"
-
-    def fetchResults(self, search, context, feedback):
-
-        if len(search) < 2:
-            return
-
-        url = "{}{}".format(self.SEARCH_URL, search)
-        self.info("Search url {}".format(url))
-        nam = NetworkAccessManager()
-        try:
-
-            headers = {b"User-Agent": self.USER_AGENT}
-            # use BLOCKING request, as fetchResults already has it's own thread!
-            (response, content) = nam.request(url, headers=headers, blocking=True)
-
-            if (
-                response.status_code == 200
-            ):  # other codes are handled by NetworkAccessManager
-                content_string = content.decode("utf-8")
-                locations = json.loads(content_string)
-
-                # loop on features in json collection
-                for loc in locations["features"]:
-
-                    result = QgsLocatorResult()
-                    result.filter = self
-                    label = loc["properties"]["label"]
-                    if loc["properties"]["type"] == "municipality":
-                        # add city code to label
-                        label += " " + loc["properties"]["citycode"]
-                    result.displayString = label
-                    result.group = loc["properties"]["type"]
-                    # use the json full item as userData, so all info is in it:
-                    result.userData = loc
-                    self.resultFetched.emit(result)
-
-        except RequestsException as err:
-            # Handle exception..
-            self.info(err)
-            self.resultProblem.emit("{}".format(err))
-
-    def triggerResult(self, result):
-        self.info("UserClick: {}".format(result.displayString))
-        doc = result.userData
-        x = doc["geometry"]["coordinates"][0]
-        y = doc["geometry"]["coordinates"][1]
-
-        centerPoint = QgsPointXY(x, y)
-
-        dest_crs = QgsProject.instance().crs()
-        results_crs = QgsCoordinateReferenceSystem(
-            4326, QgsCoordinateReferenceSystem.PostgisCrsId
-        )
-        aTransform = QgsCoordinateTransform(
-            results_crs, dest_crs, QgsProject.instance()
-        )
-        centerPointProjected = aTransform.transform(centerPoint)
-        aTransform.transform(centerPoint)
-
-        # centers to adress coordinates
-        self.iface.mapCanvas().setCenter(centerPointProjected)
-
-        # zoom policy has we don't have extent in the results
-        scale = 25000
-
-        type_adress = doc["properties"]["type"]
-
-        if type_adress == "housenumber":
-            scale = 2000
-        elif type_adress == "street":
-            scale = 5000
-        elif type_adress == "locality":
-            scale = 5000
-
-        # finally zoom actually
-        self.iface.mapCanvas().zoomScale(scale)
-        self.iface.mapCanvas().refresh()
-
-    def info(self, msg=""):
-        QgsMessageLog.logMessage(
-            "{} {}".format(self.__class__.__name__, msg), "LocatorFilter", Qgis.Info
-        )
+        :param err: error message
+        :type err: [type]
+        """
+        self.log(message=err, log_level=1, push=True)
