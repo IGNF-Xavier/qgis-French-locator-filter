295beebf03119607461d5fdf0cb41759018f084d
Julien M <julien.moura@oslandia.com>
2021-06-01 18:00:51 +0200

    Add settings dialog and translation

---
+++ french_locator_filter/resources/i18n/french_locator_filter_fr.ts
@@ -0,0 +1,69 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="fr_FR" sourcelanguage="">
+<context>
+    <name>NetworkRequestsManager</name>
+    <message>
+        <location filename="../../toolbelt/network_manager.py" line="94"/>
+        <source>Request to {} succeeded.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dlg_settings</name>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="26"/>
+        <source>Settings</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="88"/>
+        <source>Features</source>
+        <translation>Fonctionnalités</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="94"/>
+        <source>HTTP user-agent:</source>
+        <translation>HTPP user-agent :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="101"/>
+        <source>Request URL:</source>
+        <translation>Base de l&apos;URL de requête :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="108"/>
+        <source>Request parameters:</source>
+        <translation>Paramètres de la requête :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="170"/>
+        <source>Miscellaneous</source>
+        <translation>Divers</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="179"/>
+        <source>Enable debug mode.</source>
+        <translation>Activer le mode debug.</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="188"/>
+        <source>Debug mode (degraded performances)</source>
+        <translation>Activer le mode debug (performances dégradées)</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="208"/>
+        <source>Version used to save settings:</source>
+        <translation>Paramaètres sauvegardés avec la version :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="227"/>
+        <source>Report an issue</source>
+        <translation>Signaler une anomalie</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="238"/>
+        <source>Help</source>
+        <translation>Aide en ligne</translation>
+    </message>
+</context>
+</TS>
--- french_locator_filter/toolbelt/__init__.py
+++ french_locator_filter/toolbelt/__init__.py
@@ -2,3 +2,4 @@
 from .log_handler import PlgLogger  # noqa: F401
 from .network_manager import NetworkRequestsManager  # noqa: F401
 from .preferences import PlgOptionsManager  # noqa: F401
+from .translator import PlgTranslator  # noqa: F401
