25156676318ed31880fad6c6b1e49e16f99db216
Julien M. <julien.moura@oslandia.com>
2025-04-24 12:52:10 +0200

    fix(tests): use latest metadata test fomr the templater to handle full semver version

--- french_locator_filter/__about__.py
+++ french_locator_filter/__about__.py
@@ -17,8 +17,7 @@ from pathlib import Path
 # ############################################################################
 # ########## Globals ###############
 # ##################################
-
-__all__ = [
+__all__: list = [
     "__author__",
     "__copyright__",
     "__email__",
@@ -29,14 +28,14 @@ __all__ = [
     "__version__",
 ]
 
-DIR_PLUGIN_ROOT = Path(__file__).parent
-PLG_METADATA_FILE = DIR_PLUGIN_ROOT.resolve() / "metadata.txt"
+
+DIR_PLUGIN_ROOT: Path = Path(__file__).parent
+PLG_METADATA_FILE: Path = DIR_PLUGIN_ROOT.resolve() / "metadata.txt"
+
 
 # ############################################################################
 # ########## Functions #############
 # ##################################
-
-
 def plugin_metadata_as_dict() -> dict:
     """Read plugin metadata.txt and returns it as a Python dict.
 
@@ -59,33 +58,33 @@ def plugin_metadata_as_dict() -> dict:
 # ##################################
 
 # store full metadata.txt as dict into a var
-__plugin_md__ = plugin_metadata_as_dict()
+__plugin_md__: dict = plugin_metadata_as_dict()
 
-__author__ = __plugin_md__.get("general").get("author")
-__copyright__ = "2014 - {0}, {1}".format(date.today().year, __author__)
-__email__ = __plugin_md__.get("general").get("email")
+__author__: str = __plugin_md__.get("general").get("author")
+__copyright__: str = "2025 - {0}, {1}".format(date.today().year, __author__)
+__email__: str = __plugin_md__.get("general").get("email")
 __icon_path__: Path = DIR_PLUGIN_ROOT.resolve() / __plugin_md__.get("general").get(
     "icon"
 )
-__keywords__ = [
+__keywords__: list = [
     t.strip() for t in __plugin_md__.get("general").get("repository").split("tags")
 ]
-__license__ = "GPL-2.0"
-__summary__ = "{}\n{}".format(
+__license__: str = "GPLv2+"
+__summary__: str = "{}\n{}".format(
     __plugin_md__.get("general").get("description"),
     __plugin_md__.get("general").get("about"),
 )
 
-__title__ = __plugin_md__.get("general").get("name")
-__title_clean__ = "".join(e for e in __title__ if e.isalnum())
+__title__: str = __plugin_md__.get("general").get("name")
+__title_clean__: str = "".join(e for e in __title__ if e.isalnum())
 
-__uri_homepage__ = __plugin_md__.get("general").get("homepage")
-__uri_repository__ = __plugin_md__.get("general").get("repository")
-__uri_tracker__ = __plugin_md__.get("general").get("tracker")
-__uri__ = __uri_repository__
+__uri_homepage__: str = __plugin_md__.get("general").get("homepage")
+__uri_repository__: str = __plugin_md__.get("general").get("repository")
+__uri_tracker__: str = __plugin_md__.get("general").get("tracker")
+__uri__: str = __uri_repository__
 
-__version__ = __plugin_md__.get("general").get("version")
-__version_info__ = tuple(
+__version__: str = __plugin_md__.get("general").get("version")
+__version_info__: tuple = tuple(
     [
         int(num) if num.isdigit() else num
         for num in __version__.replace("-", ".", 1).split(".")
@@ -99,16 +98,16 @@ if __name__ == "__main__":
     plugin_md = plugin_metadata_as_dict()
     assert isinstance(plugin_md, dict)
     assert plugin_md.get("general").get("name") == __title__
-    print("Plugin: " + __title__)
-    print("Plugin (clean): " + __title_clean__)
-    print("By: " + __author__)
-    print("Version: " + __version__)
-    print("Description: " + __summary__)
+    print(f"Plugin: {__title__}")
+    print(f"By: {__author__}")
+    print(f"Version: {__version__}")
+    print(f"Description: {__summary__}")
+    print(f"Icon: {__icon_path__}")
     print(
         "For: %s > QGIS > %s"
         % (
             plugin_md.get("general").get("qgisminimumversion"),
-            plugin_md.get("general").get("qgismaximumversion", "3.99"),
+            plugin_md.get("general").get("qgismaximumversion"),
         )
     )
     print(__title_clean__)
